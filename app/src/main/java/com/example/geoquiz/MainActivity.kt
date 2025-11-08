ed3a5fbf33ec3abf929a1ebe691cb070001576f5
Start project <nik_orlov77@mail.ru>
2025-11-08 22:10:33 +0700

    AddTF

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -7,12 +7,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -57,7 +61,8 @@ fun GeoQuizApp() {
     var currentQuestionIndex = 0
 
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
             text = "GeoQuiz",
@@ -81,6 +86,48 @@ fun GeoQuizApp() {
             textAlign = TextAlign.Center,
             color = Color.DarkGray
         )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 32.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            // Кнопка True
+            Button(
+                onClick = { /* TODO: Обработка ответа True */ },
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(end = 60.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF7041D2)
+                ),
+                shape = RoundedCornerShape(6.dp)
+            ) {
+                Text(
+                    text = "TRUE",
+                    color = Color.White,
+                    fontSize = 16.sp
+                )
+            }
+
+            // Кнопка False
+            Button(
+                onClick = { /* TODO: Обработка ответа False */ },
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 60.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF7041D2)
+                ),
+                shape = RoundedCornerShape(6.dp)
+            ) {
+                Text(
+                    text = "FALSE",
+                    color = Color.White,
+                    fontSize = 16.sp
+                )
+            }
+        }
     }
 }
 
