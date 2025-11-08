39995125d67f6f6251bf01a58e335199275b2f09
Start project <nik_orlov77@mail.ru>
2025-11-08 11:00:12 +0700

    StartProject

---
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.geoquiz
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.geoquiz.ui.theme.GeoQuizTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GeoQuizTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GeoQuizApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GeoQuizApp() {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = "GeoQuiz",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(70.dp)
+                .background(Color(0xFF7041D2))
+                .padding(start = 24.dp, top = 22.dp, bottom = 16.dp),
+            color = Color.White,
+            fontSize = 22.sp,
+            textAlign = TextAlign.Start
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GeoQuizPreview() {
+    GeoQuizTheme {
+        GeoQuizApp()
+    }
+}
